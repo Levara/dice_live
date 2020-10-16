4a08afd49615f82fd75e1bd6b3094dfba2851f3b
levara <hrvoje.leventic@gmail.com>
2020-10-16 13:42:55 +0200

    Add support for multiple dices

--- dice.py
+++ dice.py
@@ -3,12 +3,15 @@ import os,sys
 
 def main(): 
     if len(sys.argv) == 1:
-        num_sides = int(input("Enter number of sides for a dice:"))
+        num_sides = int(input("Enter number of sides for a dice: "))
+        num_dices = int(input("Enter number of dices: "))
     else: 
         print(f"Arguments: {sys.argv}")
         num_sides = int(sys.argv[1])
-    roll = random.randint(1,num_sides)
-    print("Dice roll is %d"%(roll))
+        num_dices = int(sys.argv[2])
+    for i in range(num_dices):
+        roll = random.randint(1,num_sides)
+        print("Dice [%d] roll is %d"%(i+1, roll))
 
 if __name__ == "__main__":
     main()
